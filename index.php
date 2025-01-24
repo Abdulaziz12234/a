6a0d6872f864f9d9bc7bb08ad34a3dbc46bc602e
Abdulaziz12234 <xx12t4m@gmail.com>
2025-01-24 18:44:51 +0300

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,18 +1,31 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
+
+  if (isset($_COOKIE['user_logged_in']) && $_COOKIE['user_logged_in'] === 'true') {
+        $_SESSION['logged_in'] = true;
+        $_SESSION['username'] = $_COOKIE['username'];
+    } else {
+        header("Location: login_register.php");
+        exit();
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <title>Sunset Hotel</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <link rel="stylesheet" href="css/animate.css">
     <link rel="stylesheet" href="css/icomoon.css">
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/superfish.css">
     <link rel="stylesheet" href="css/style.css">
-    
-	<link rel="icon" href="Initial H Letter Real Estate Logo Design.png" type="image/x-icon">
-
+    <link rel="icon" href="Initial H Letter Real Estate Logo Design.png" type="image/x-icon">
   </head>
   <body>
     <div id="fh5co-wrapper">
@@ -22,15 +35,19 @@
             <div class="container">
               <div class="nav-header">
                 <a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
-				<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
-				<h1 id="fh5co-logo"><a href="index.html">Sunset Hotel</a></h1>
-                    
-                  </a>
-                </h1>
+                <a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
+                <h1 id="fh5co-logo"><a href="index.php">Sunset Hotel</a></h1>
                 <nav id="fh5co-menu-wrap" role="navigation">
                   <ul class="sf-menu" id="fh5co-primary-menu">
-                    <li class="active"><a href="index.html">Home</a></li>
-                    <li><a href="register.html">Register</a></li>
+                    <li class="active"><a href="index.php">Home</a></li>
+                    <?php if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true): ?>
+                      <li><a href="register.html">Register</a></li>
+                    <?php endif; ?>
+                    <?php if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true): ?>
+                      <li><a href="logout.php">Logout</a></li>
+                      <li><a href="your_logs.php">Welcome, <?php echo htmlspecialchars($_SESSION['username']); ?>!</a></li>
+
+                    <?php endif; ?>
                   </ul>
                 </nav>
               </div>
@@ -41,20 +58,20 @@
         <div class="fh5co-hero">
           <div class="fh5co-overlay"></div>
           <div class="fh5co-cover text-center" data-stellar-background-ratio="0.5" style="background-image: url(https://cf.bstatic.com/xdata/images/hotel/max1024x768/76526205.jpg?k=92fc8281dd5bad6e06474ba61f01f00a29dcce5470db9af5bd081a9bc41b5ffe&o=&hp=1);">
-            <div class="desc animate-box">
-              <h2>Rooms designed for you</h2>
-              <span><a class="btn btn-primary btn-lg" href="reservation.html">Book now</a></span>
-            </div>
+          <div class="desc animate-box">
+  <h2>Rooms designed for you</h2>
+  <span><a class="btn btn-primary btn-lg" href="reservation.php">Book now</a></span>
+</div>
+
           </div>
         </div>
-		
 
         <div class="fh5co-listing">
           <div class="container">
             <div class="row">
-              <!-- First row -->
-              <div class="col-md-4 col-sm-4 fh5co-item-wrap">
-                <a class="fh5co-listing-item" href="reservation.html">
+               <!-- First row -->
+               <div class="col-md-4 col-sm-4 fh5co-item-wrap">
+                <a class="fh5co-listing-item" href="reservation.php">
                   <img src="https://www.amsterdamforesthotel.com/images/rooms/single/single-room-amsterdam-forest-hotel-1.jpg" alt="Single Room" class="img-responsive">
                   <div class="fh5co-listing-copy">
                     <h2>Single Room</h2>
@@ -66,7 +83,7 @@
               </div>
 
               <div class="col-md-4 col-sm-4 fh5co-item-wrap">
-                <a class="fh5co-listing-item" href="reservation.html">
+                <a class="fh5co-listing-item" href="reservation.php">
                   <img src="https://www.twahotel.com/sites/default/files/2019-07/standard-king-large.jpg" alt="Standard Room" class="img-responsive">
                   <div class="fh5co-listing-copy">
                     <h2>Standard Room</h2>
@@ -78,7 +95,7 @@
               </div>
 
               <div class="col-md-4 col-sm-4 fh5co-item-wrap">
-                <a class="fh5co-listing-item" href="reservation.html">
+                <a class="fh5co-listing-item" href="reservation.php">
                   <img src="https://www.mulberrynepal.com/wp-content/uploads/2017/04/business-suite-7.jpg" alt="Business Suite" class="img-responsive">
                   <div class="fh5co-listing-copy">
                     <h2>Business Suite</h2>
@@ -93,7 +110,7 @@
             <div class="row">
               <!-- Second row -->
               <div class="col-md-4 col-sm-4 fh5co-item-wrap">
-                <a class="fh5co-listing-item" href="reservation.html">
+                <a class="fh5co-listing-item" href="reservation.php">
                   <img src="https://cdn.webhotelier.net/photos/w=1920/michel-presfp/L722887.jpg" alt="Junior Suite" class="img-responsive">
                   <div class="fh5co-listing-copy">
                     <h2>Junior Suite</h2>
@@ -105,7 +122,7 @@
               </div>
 
               <div class="col-md-4 col-sm-4 fh5co-item-wrap">
-                <a class="fh5co-listing-item" href="reservation.html">
+                <a class="fh5co-listing-item" href="reservation.php">
                   <img src="https://focus.independent.ie/thumbor/501JuQ4mUx2AQD4FnYXHl-5-FoA=/0x17:1700x954/960x640/prod-mh-ireland/0c86e7a0-aa0a-11ed-9c83-0210609a3fe2.jpeg" alt="Family Room" class="img-responsive">
                   <div class="fh5co-listing-copy">
                     <h2>Family Room</h2>
@@ -117,7 +134,7 @@
               </div>
 
               <div class="col-md-4 col-sm-4 fh5co-item-wrap">
-                <a class="fh5co-listing-item" href="reservation.html">
+                <a class="fh5co-listing-item" href="reservation.php">
                   <img src="https://res.cloudinary.com/traveltripperweb/image/upload/c_limit,f_auto,h_2500,q_auto,w_2500/v1580455406/vahre6fc9gt8ygcdmkuu.jpg" alt="Double Room" class="img-responsive">
                   <div class="fh5co-listing-copy">
                     <h2>Double Room</h2>
@@ -162,14 +179,20 @@
           </div>
         </div>
 
+        
+                </div>
+              </div>
+            </div>
+          </div>
+        <!-- Footer -->
         <footer>
           <div id="footer">
             <div class="container">
               <div class="row">
                 <div class="col-md-6 col-md-offset-3 text-center">
                   <p style="font-size: 14px; line-height: 1.6;">
-                    <a href="#" style="font-size: 18px; font-weight: bold; color: #200d97;">Sunset Hotel</a>
-                    <span style="font-size: 14px; color: #ffffff;">All Rights Reserved.</span><br>
+                             <a href="#" style="font-size: 18px; font-weight: bold; color:rgb(144, 200, 238);">Sunset Hotel</a>
+                  <span style="font-size: 14px; color: #ffffff;">All Rights Reserved.</span><br>
                     <span style="font-size: 14px; color: #ffffff;">Made by UQU Students</span><br><br>
                     <span style="font-size: 14px; color: #ffffff;">Contact with us</span><br>
                     <i class="icon-mail" style="font-size: 16px; color: #ffffff;"> </i>
